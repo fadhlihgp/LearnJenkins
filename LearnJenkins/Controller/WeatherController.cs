3b6b8ca4b5a6ae143f84c64c0ca934d44cd24d6c
fadhlihgp <fadhlihgp@gmail.com>
2025-02-15 13:42:52 +0700

    initial

---
+++ LearnJenkins/Controller/WeatherController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnJenkins.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherController : ControllerBase
+{
+    private string[] _summaries;
+
+    public WeatherController()
+    {
+        _summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+    }
+
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+    
+    [HttpGet("list")]
+    public IActionResult List()
+    {
+        
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    _summaries[Random.Shared.Next(_summaries.Length)]
+                ))
+            .ToArray();
+        return Ok(forecast);
+    }
+}
